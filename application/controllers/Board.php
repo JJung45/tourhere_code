8dad65d374117127dd3d91253fe688d774952d6b
wjdgkrud <753cindy@naver.com>
2019-03-08 17:30:09 +0900

    update mypage

--- application/controllers/Board.php
+++ application/controllers/Board.php
@@ -13,37 +13,22 @@ class Board extends CI_Controller
         $this->load->model('Board_Model');
     }
 
+
     public function main()
     {
         $this->load->view('head');
         $this->load->view('mainnav');
 
-        $board = $this->Board_Model->getAll();
+        $page = $this->input->post('pagenum') ?? 0;
+        $boards = $this->Board_Model->get_page(intval($page));
 
-        $boards = array();
+        $this->load->view('board',[
+           'boards' => $boards
+        ]);
 
-        if ($board != 0) {
-            $i = 0;
-            $boards['bidx'] = array();
-            $boards['id'] = array();
-            $boards['img'] = array();
-            $boards['txt'] = array();
-            $boards['created'] = array();
-            foreach ($board as $board) {
-                $boards['bidx'][$i] = $board->bidx;
-                $boards['id'][$i] = $board->id;
-                $boards['img'][$i] = $board->img;
-                $boards['txt'][$i] = $board->txt;
-                $boards['created'][$i] = $board->created;
-                $i++;
-            }
+       $this->load->view('footer');
 
-            $this->load->view('board', $boards);
-            $this->load->view('footer');
-        } else {
-            $this->load->view('board');
-            $this->load->view('footer');
-        }
+
     }
 
     //무한스크롤 시도
@@ -77,29 +62,12 @@ class Board extends CI_Controller
         ));
 
 
-        $boards=array();
+        $this->load->view('board',[
+            'board' => $board
+        ]);
 
-        if ($board != 0) {
-            $i=0;
-            $boards['bidx']=array();
-            $boards['id']=array();
-            $boards['img']=array();
-            $boards['txt']=array();
-            $boards['created']=array();
-            foreach ($board as $board) {
-                $boards['bidx'][$i] =$board->bidx;
-                $boards['id'][$i] =$board->id;
-                $boards['img'][$i] =$board->img;
-                $boards['txt'][$i] = $board->txt;
-                $boards['created'][$i] = $board->created;
-                $i++;
-            }
-            $this->load->view('mypage',$boards);
-            $this->load->view('footer');
-        }else{
-            $this->load->view('mypage');
-            $this->load->view('footer');
-        }
+
+        $this->load->view('footer');
 
     }
 
@@ -139,6 +107,113 @@ class Board extends CI_Controller
 //
 //        echo $this->pagination->create_links();
 
+        $this->load->view('footer');
+    }
+
+}
+
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+class Board extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function main()
+    {
+        $this->load->view('head');
+        $this->load->view('mainnav');
+
+        $this->load->model('Board_Model');
+
+        $page = $this->input->post('pagenum') ?? 0;
+        $boards = $this->Board_Model->get_page(intval($page));
+
+        $this->load->view('board', [
+            'boards' => $boards
+        ]);
+        $this->load->view('footer');
+    }
+
+    //무한스크롤 시도
+    public function page()
+    {
+
+        $this->load->model('Board_Model');
+
+        $page = $this->input->post('pagenum');
+        $data['clien'] = $this->Board_Model->get_page(intval($page));
+        $this->load->view('/board/main', $data);
+    }
+
+    public function boardList()
+    {
+
+        $this->load->model('Board_Model');
+
+        $page = $this->input->get('pagenum') ?? 0;
+        $data['boards'] = $this->Board_Model->get_page($page);
+
+        $this->load->view('/ajax/board_list', $data);
+    }
+
+
+    public function view()
+    {//작업 미완료
+        $this->load->view('head');
+
+
+        $this->load->view('view');
+        $this->load->view('footer');
+    }
+
+
+    public function mypage()
+    {
+
+        $this->load->view('head');
+
+        $this->load->model('Board_Model');
+        $board = $this->Board_Model->getById(array(
+            'id' => $this->session->userdata('userId')
+        ));
+
+
+        $boards = array();
+
+        if ($board != 0) {
+            $i = 0;
+            $boards['bidx'] = array();
+            $boards['id'] = array();
+            $boards['img'] = array();
+            $boards['txt'] = array();
+            $boards['created'] = array();
+            foreach ($board as $board) {
+                $boards['bidx'][$i] = $board->bidx;
+                $boards['id'][$i] = $board->id;
+                $boards['img'][$i] = $board->img;
+                $boards['txt'][$i] = $board->txt;
+                $boards['created'][$i] = $board->created;
+                $i++;
+            }
+            $this->load->view('mypage', $boards);
+            $this->load->view('footer');
+        } else {
+            $this->load->view('mypage');
+            $this->load->view('footer');
+        }
+
+    }
+
+    public function notification()
+    {
+        $this->load->view('head');
+        $this->load->view('notification');
         $this->load->view('footer');
     }
 
