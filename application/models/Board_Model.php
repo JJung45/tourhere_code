972963a6c981efe3688133864a5e0aebda8735b5
wjdgkrud <753cindy@naver.com>
2019-03-11 17:50:17 +0900

    board_model add

--- application/models/Board_Model.php
+++ application/models/Board_Model.php
@@ -1,6 +1,10 @@
 <?php if (!defined('BASEPATH')) {    exit('No direct script access allowed'); }
 class Board_Model extends CI_Model {
     function add($data){
+
+        $this->db->set('created_at','NOW()',FALSE);
+        $this->db->insert('save_upload',$data);
+
         $this->db->set('created','NOW()',FALSE);
         $this->db->insert('board',$data);
     }
